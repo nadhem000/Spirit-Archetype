e03c9d8c95b97f183c794bc2bf8aebee0c6ea2ce
MejriZiad <mejri.ziad@gmail.com>
2025-10-25 15:28:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Spiritual Guide Test PWA - Enhanced Version
-const CACHE_NAME = 'spiritual-guide-v1.2.5'; // Increment version
+const CACHE_NAME = 'spiritual-guide-v1.2.6'; // Increment version
 const urlsToCache = [
   '/',
   '/index.html',
