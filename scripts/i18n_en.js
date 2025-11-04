0d422c7bcecd7092ffa966af350c8e0fef17e138
MejriZiad <mejri.ziad@gmail.com>
2025-11-04 07:01:51 +0100

    Add files via upload

--- scripts/i18n_en.js
+++ scripts/i18n_en.js
@@ -158,7 +158,7 @@ const translations_en = {
 		},
         footer: {
             developedBy: "Developed by Mejri Ziad",
-            version: "spiritual-guide-v2.6.0",
+            version: "spiritual-guide-v2.6.1",
             project: "Atlantis Platform"
 		},
 		music: {
